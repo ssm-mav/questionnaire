3835e5bed2b405d9ce0b83b9dbfd5388509ec992
StephenTao <stephen_huang_@hotmail.com>
2016-06-25 17:04:20 +0800

    init package

---
+++ src/main/java/com/ssm/questionnaire/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.ssm.questionnaire.controller;
+
+import com.ssm.questionnaire.base.BaseController;
+
+public class UserController extends BaseController {
+
+}
---
+++ src/main/java/com/ssm/questionnaire/dao/mybatis/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package com.ssm.questionnaire.dao.mybatis;
+
+import com.ssm.questionnaire.base.BaseDao;
+import com.ssm.questionnaire.dao.UserDao;
+
+public class UserDaoImpl extends BaseDao implements UserDao  {
+
+}
