0a550cf7f523dbeadcefc0b5e82f202a898f1afa
unknown <yangwen116102@163.com>
2016-06-25 19:13:26 +0800

    issue

--- src/main/java/com/ssm/questionnaire/controller/UserController.java
+++ src/main/java/com/ssm/questionnaire/controller/UserController.java
@@ -1,7 +1,19 @@
 package com.ssm.questionnaire.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.ssm.questionnaire.base.BaseController;
 
+@Controller
 public class UserController extends BaseController {
 
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHi() {
+        System.out.println("hi");
+        return "ok";
+    }
 }
