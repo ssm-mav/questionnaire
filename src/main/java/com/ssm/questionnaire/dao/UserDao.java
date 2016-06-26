e95226b747d31c53a68b18dde422975fb38c38a3
StephenTao <stephen_huang_@hotmail.com>
2016-06-26 23:27:27 +0800

    add dao impl

--- src/main/java/com/ssm/questionnaire/dao/UserDao.java
+++ src/main/java/com/ssm/questionnaire/dao/UserDao.java
@@ -1,12 +1,14 @@
 package com.ssm.questionnaire.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import com.ssm.questionnaire.base.IBaseDao;
 import com.ssm.questionnaire.model.User;
 
 @Repository
-public interface UserDao {
+public interface UserDao extends IBaseDao<User, Integer> {
 
-    User getById(int id);
-
+	List<User> find();
 }
--- src/main/java/com/ssm/questionnaire/dao/mybatis/UserDaoImpl.java
+++ src/main/java/com/ssm/questionnaire/dao/mybatis/UserDaoImpl.java
@@ -1,14 +1,18 @@
 package com.ssm.questionnaire.dao.mybatis;
 
+import java.util.List;
+
 import com.ssm.questionnaire.base.BaseDao;
 import com.ssm.questionnaire.dao.UserDao;
 import com.ssm.questionnaire.model.User;
 
-public class UserDaoImpl extends BaseDao implements UserDao  {
+public class UserDaoImpl extends BaseDao<User, Integer> implements UserDao  {
 
-    public User getById(int id) {
-        
-        return null;
-    }
+	public static final String CLASS_NAME = User.class.getName();
+	
+	public List<User> find() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
